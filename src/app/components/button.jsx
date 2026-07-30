77d872727250c6a501b144e22a77282bbca05e92
Juris <J.Cavniss@wlv.ac.uk>
2026-07-31 02:21:50 +0300

    Added button and logo design, working on main page/log off user

---
+++ src/app/components/button.jsx
@@ -0,0 +1,3 @@
+export function Button({ children, variant = "primary", className = "", ...props }) {
+  return <button className={"button " + variant + " " + className} {...props}>{children}</button>;
+}
